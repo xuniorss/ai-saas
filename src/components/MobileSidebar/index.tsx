41e46900f5e40eb55924be7e7966f2a5c2e4ff26
xuniorss <fortunatojunior2020@gmail.com>
2023-07-19 16:28:39 -0300

    fix:Hydration-Error

--- src/components/MobileSidebar/index.tsx
+++ src/components/MobileSidebar/index.tsx
@@ -1,11 +1,18 @@
 'use client'
 
 import { Menu } from 'lucide-react'
+import { useEffect, useState } from 'react'
 import { Sidebar } from '../Sidebar'
 import { Button } from '../ui/button'
 import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet'
 
 export const MobileSidebar = () => {
+   const [isMounted, setIsMounted] = useState(false)
+
+   useEffect(() => setIsMounted(true), [])
+
+   if (!isMounted) return null
+
    return (
       <Sheet>
          <SheetTrigger>
